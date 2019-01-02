4af505eaa73b0118aae27e12a198565d6de9882a
Moshe Karten <moek613@gmail.com>
2019-01-02 15:21:49 -0500

    changed some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-const isDev = process.env.NODE_ENV === "development";
-
 module.exports = {
-  mode: isDev ? "development" : "production",
-  entry: [
-    "@babel/polyfill", // enables async-await
-    "./client/index.js"
-  ],
+  // babel-polyfill enables async-await in our client js
+  entry: ['babel-polyfill', './client/index.js'],
   output: {
     path: __dirname,
-    filename: "./public/bundle.js"
+    filename: './public/bundle.js'
   },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  devtool: "source-map",
+  context: __dirname,
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
       }
     ]
   }
-};
+}
